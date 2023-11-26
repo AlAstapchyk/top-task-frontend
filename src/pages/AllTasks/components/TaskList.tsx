9f385e8cdd4d98ba46ed028711c93bbe79f1873d
Alexander Astapchyk <alastapchyk@gmail.com>
2023-11-26 09:27:45 +0100

    initial isOpen argument for list

--- src/pages/AllTasks/components/TaskList.tsx
+++ src/pages/AllTasks/components/TaskList.tsx
@@ -9,13 +9,15 @@ import { HamburgerSvg } from "../../../../public/assets/svgs";
 interface TaskListProps {
   priority?: string;
   tasks: Task[];
+  initialIsOpen?: boolean;
 }
 const TaskList: React.FC<TaskListProps> = ({
   priority,
   tasks,
+  initialIsOpen,
 }: TaskListProps) => {
   const rightPanel = useAppSelector((state) => state.rightPanel);
-  const [isOpen, setIsOpen] = useState<boolean>(true);
+  const [isOpen, setIsOpen] = useState<boolean>(initialIsOpen !== false);
 
   return (
     <div className={`task-list ${priority}`}>
@@ -32,37 +34,38 @@ const TaskList: React.FC<TaskListProps> = ({
       <Droppable droppableId={priority || ""}>
         {(provided) => (
           <ul ref={provided.innerRef} {...provided.droppableProps}>
-            {isOpen && tasks
-              .filter((task) => task.priority === `${priority}`)
-              .sort((a, b) => a.priorityPosition - b.priorityPosition)
-              .map((task, index) => (
-                <Draggable
-                  key={task.id}
-                  draggableId={task.id.toString()}
-                  index={index}
-                >
-                  {(provided) => (
-                    <li
-                      className="draggable-task-item"
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                    >
-                      <TaskItem
-                        task={task}
-                        additionalClassName={
-                          String(
-                            task.id === rightPanel.id &&
-                              rightPanel.type === RightPanelType.showTask
-                              ? "active "
-                              : ""
-                          ) + (task.isComplete ? "complete" : "")
-                        }
-                      />
-                    </li>
-                  )}
-                </Draggable>
-              ))}
+            {isOpen &&
+              tasks
+                .filter((task) => task.priority === `${priority}`)
+                .sort((a, b) => a.priorityPosition - b.priorityPosition)
+                .map((task, index) => (
+                  <Draggable
+                    key={task.id}
+                    draggableId={task.id.toString()}
+                    index={index}
+                  >
+                    {(provided) => (
+                      <li
+                        className="draggable-task-item"
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        <TaskItem
+                          task={task}
+                          additionalClassName={
+                            String(
+                              task.id === rightPanel.id &&
+                                rightPanel.type === RightPanelType.showTask
+                                ? "active "
+                                : ""
+                            ) + (task.isComplete ? "complete" : "")
+                          }
+                        />
+                      </li>
+                    )}
+                  </Draggable>
+                ))}
             {provided.placeholder}
           </ul>
         )}
